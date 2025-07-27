f86a82e6892adbf0208bb6b1858c3904057383fe
Locktix <alanplokain@hotmail.com>
2025-07-27 20:57:00 +0200

    v1.3.1

--- script.js
+++ script.js
@@ -583,7 +583,7 @@ async function displayCurrentPage() {
     // Re-trigger l'animation de transition de page
     pokemonGrid.style.animation = 'none';
     pokemonGrid.offsetHeight; // Force reflow
-    pokemonGrid.style.animation = 'pageSlideIn 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94)';
+    pokemonGrid.style.animation = 'pageZoomIn 0.3s cubic-bezier(0.34, 1.56, 0.64, 1)';
 
 }
 
